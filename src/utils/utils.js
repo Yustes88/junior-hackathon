dedb2a8174507f05965a2397abfcd8804ca86056
Yustes88 <iuliiaobr.io@gmail.com>
2023-11-11 21:15:04 +0300

    instead of a removed object add a empty object so the empty cell is always random

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 export const shuffle = (images) => {
     const randomIndex = Math.floor(Math.random() * images.length);
     const newArr = new Array(...images)
-    newArr.splice(randomIndex, 1);
+    newArr.splice(randomIndex, 1, {});
     return newArr.sort(() => Math.random() -.5)
 }
 
