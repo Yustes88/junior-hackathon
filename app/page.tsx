a495187bfe3e1c0d57bc0f79d80862fd89a88351
Yustes88 <iuliiaobr.io@gmail.com>
2023-11-11 15:49:25 +0300

    centeres game field

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="gap-4 p-24">
+    <main className="p-24 flex flex-col items-center justify-center">
       <div className="text-white text-md">
         <Link href={'/gallery'}>Go back to gallery</Link>
       </div>
-      <div className='flex justify-center'>
+      <div className='flex justify-center w-3/5 h-3/5'>
         <GameField/>
       </div>
     </main>
