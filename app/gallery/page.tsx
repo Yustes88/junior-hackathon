93bbaf4c189a1f828776b6bb3ed6d7eefcf6da59
Yustes88 <iuliiaobr.io@gmail.com>
2023-11-11 12:41:35 +0300

    adds gallery page

---
+++ app/gallery/page.tsx
@@ -0,0 +1,12 @@
+export default function Gallery() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className='text-7xl'>
+        Gallery
+        </div>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit culpa laboriosam nostrum rem sed iusto delectus id repellendus voluptatibus tempora eveniet repudiandae, eos eum voluptatum. Modi numquam magni dolores culpa?
+        </div>
+      </main>
+    )
+  }
