91ed591c7b003c04daee30252b829f8de5539a7b
Yustes88 <iuliiaobr.io@gmail.com>
2023-11-11 18:15:53 +0300

    adds header component

--- src/Layout.js
+++ src/Layout.js
@@ -1,17 +1,14 @@
 import { useLocation } from "react-router-dom";
 import Outlet from "./Outlet";
+import Header from "./components/Header";
 
 function Layout() {
     const location = useLocation();
 
     return (
       <div>
-    <div className="App w-full h-full">
-      <header>
-        <h1>
-        Hello world!
-        </h1>
-      </header>
+    <div className="App">
+      <Header/>
     </div>
         <main>
           <Outlet location={location} />
