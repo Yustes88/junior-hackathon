5ae3c9fd0f35070c4b590b2912bbc50b3cf67c88
Yustes88 <iuliiaobr.io@gmail.com>
2023-11-11 19:04:51 +0300

    shuffles images with value and indexes

--- src/components/field/Field.js
+++ src/components/field/Field.js
@@ -1,6 +1,15 @@
 import Overlay from "../overlay/Overlay";
 
 function Field() {
+    const shuffleImages = () => 
+    new Array(16)
+    .fill()
+    .map((_,i) => i+1)
+    .sort(() => Math.random() -.5)
+    .map((x,i) => ({value : x , index : i}))
+    console.log(shuffleImages())
+
+
     return(
         <div className="relative">
             <div className="grid cell relative overflow-hidden rounded-sm border-4 border-solid border-gray-600">
