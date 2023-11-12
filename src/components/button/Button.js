d7815cd467a4f57777bb2796b08f920cfd10134a
Yustes88 <iuliiaobr.io@gmail.com>
2023-11-12 14:13:42 +0300

    adds go to gallery button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -8,7 +8,7 @@ export function Button({text, onClick}) {
         
           return (
             <Fragment>
-              <button type="button" onClick={button.onClick} className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              <button type="button" onClick={button.onClick} className="rounded-md mr-2 bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
 >{button.text}</button>
             </Fragment>
           );
