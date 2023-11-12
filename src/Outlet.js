6291eee52dca0442f081dc65cbc01568c5799824
Nikita Kulikov <nikita@kulikof.ru>
2023-11-12 10:35:54 +0000

    Fix build

--- src/Outlet.js
+++ src/Outlet.js
@@ -1,21 +1,21 @@
 import Gallery from "./gallery/Gallery";
-import { useState } from "react";
+import {useState} from "react";
 import GameField from "./GameField";
 import ModalStart from "./components/modals/ModalStart";
 
 function Outlet({location, open, setOpen}) {
-
     return (
-      <>
-        <div>
-          <ModalStart open={open} setOpen={setOpen}/>
-        {
-            location.pathname === '/' ?
-              <GameField/> :
-              <Gallery />
-          }
-      </>
+        <>
+            <div>
+                <ModalStart open={open} setOpen={setOpen}/>
+                {
+                    location.pathname === '/' ?
+                        <GameField/> :
+                        <Gallery/>
+                }
+            </div>
+        </>
     );
-  }
+}
 
-  export default Outlet;
+export default Outlet;
